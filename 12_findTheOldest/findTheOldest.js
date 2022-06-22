8f81fe0d832bed5b72603817c19be5a05a58e98c
arvindrathnam1 <arvindrathnam1@gmail.com>
2022-06-21 23:20:25 -0400

    12 complete

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(a) {
+    let age = a.map(elem => {
+        if(elem["yearOfDeath"] == null)
+            elem["yearOfDeath"] = 2022;
+        return elem["yearOfDeath"] - elem["yearOfBirth"]
+    })
+    let ind = 0;
+    for(let i = 1; i < age.length; i++){
+        if(age[i] > age[ind])
+            ind = i;
+    }
+    return a[ind];
 };
 
 // Do not edit below this line
