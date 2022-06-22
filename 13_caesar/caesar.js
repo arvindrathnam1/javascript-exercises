e13621c6f8e692aef93df9d5263b3a7ed937c8b7
arvindrathnam1 <arvindrathnam1@gmail.com>
2022-06-21 23:37:10 -0400

    13 finished

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,6 +1,27 @@
-const caesar = function() {
-
+const caesar = function(a, b) {
+    let res = "";
+    b = b % 26;
+    for(let i = 0; i < a.length; i++){
+        if(!isLetter(a.charAt(i)))
+            res += a.charAt(i);
+        else if(isUpper(a.charAt(i)))
+            res += String.fromCharCode(((a.charCodeAt(i) + b - 65) + 26) % 26 + 65);
+        else
+            res += String.fromCharCode(((a.charCodeAt(i) + b - 97) + 26) % 26 + 97); 
+    }
+    return res;
 };
 
+function isLetter(str) {
+    return str.length === 1 && str.match(/[a-z]/i);
+}
+
+function isUpper(str) {
+    return str.length === 1 && str.match(/[A-Z]/);
+}
+
+function isLower(str) {
+    return str.length === 1 && str.match(/[a-z]/);
+}
 // Do not edit below this line
 module.exports = caesar;
