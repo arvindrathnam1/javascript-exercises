4ad2518dbc635b0a9e2de68c8952466d183a1f79
arvindrathnam1 <arvindrathnam1@gmail.com>
2022-06-21 20:45:21 -0400

    3 done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
 const reverseString = function(str) {
     let rev = "";
-    for(let i = str.len)
+    for(let i = str.length - 1; i >= 0; i--){
+        rev += str.charAt(i);
+    }
+
+    return rev;
 };
 
 // Do not edit below this line
