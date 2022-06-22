1500dcad0de2e1575ff846926afa26ab6ab9b80a
arvindrathnam1 <arvindrathnam1@gmail.com>
2022-06-21 21:01:48 -0400

    6 done

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
-
+const leapYears = function(yr) {
+    if(yr % 400 === 0)
+        return true;
+    if(yr % 100 === 0)
+        return false;
+    if(yr % 4 === 0)
+        return true;
+    return false;
 };
 
 // Do not edit below this line
