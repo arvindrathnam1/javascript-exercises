a71ea4978750c9442b878bc9e37543f9de4cc2f2
arvindrathnam1 <arvindrathnam1@gmail.com>
2022-06-21 20:54:09 -0400

    3 done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
 const removeFromArray = function() {
-
+    const arr = arguments[0];
+    const filt = arr.filter(elem => {
+        for(let i = 1; i < arguments.length; i++){
+            if(elem === arguments[i])
+                return false;
+        }
+        return true;
+    });
+    return filt;
 };
 
 // Do not edit below this line
